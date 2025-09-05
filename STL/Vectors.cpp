a617735fe160905e2b5b475436c564ac4986b9fc
Kartik Pant <kartikpant.kp69@gmail.com>
2025-09-05 22:28:20 +0530

    vector

--- STL/Vectors.cpp
+++ STL/Vectors.cpp
@@ -6,33 +6,56 @@
 using namespace std ;
 int main(){
 
-    vector<int>vec;
-    vec.push_back(2);
-    vec.emplace_back(3);
-     // to print the vector using for loop 
-for(int i=0;i<vec.size();i++){
-        cout<<vec[i]<<endl;
-    }
+//     vector<int>vec;
+//     vec.push_back(2);
+//     vec.emplace_back(3);
+//      // to print the vector using for loop 
+// for(int i=0;i<vec.size();i++){
+//         cout<<vec[i]<<endl;
+//     }
 
     
-    // to make a vector of pair elem
+//     // to make a vector of pair elem
 
-    vector<pair<int,int>>vec_pair;
-    vec_pair.push_back({1,2});
+//     vector<pair<int,int>>vec_pair;
+//     vec_pair.push_back({1,2});
 
 
-    // to print the vector using for loop 
-    for(int j=0;j<vec_pair.size();j++){
-        cout<<vec_pair[j].first<<vec_pair[j].second<<endl;
-    }
+//     // to print the vector using for loop 
+//     for(int j=0;j<vec_pair.size();j++){
+//         cout<<vec_pair[j].first<<vec_pair[j].second<<endl;
+//     }
     
 
 
-    // to make a vector or many elem with declaring a times of elem
-    vector<int>v1(5,100);
-    vector<int>v2(v1);
+//     // to make a vector or many elem with declaring a times of elem
+//     vector<int>v1(5,100);
+//     vector<int>v2(v1);
 
-    for(int z=0;z<v2.size();z++){
-        cout<<v2[z]<<endl;
-    }
+//     for(int z=0;z<v2.size();z++){
+//         cout<<v2[z]<<endl; 
+//     }
+
+
+    // iterator 
+
+    /*
+     Iterator is used to point the memory location and 
+
+     vector<int>::iterator it = v.begin()   -----> points just before the first element so we have to it++
+     vector<int>::iterator it = v.end()    -----> points just after the last element so we have to it--
+
+    
+    */
+ vector<int>v;
+ v.push_back(6);
+ v.push_back(10);
+ v.push_back(9);
+
+ vector<int>::iterator it;
+ for(it=v.begin();it!=v.end();++it){
+    cout<<*it<<" ";
+ }
+
+
 }
---
+++ STL/tempCodeRunnerFile.cpp
@@ -0,0 +1,30 @@
+ vector<int>vec;
+//     vec.push_back(2);
+//     vec.emplace_back(3);
+//      // to print the vector using for loop 
+// for(int i=0;i<vec.size();i++){
+//         cout<<vec[i]<<endl;
+//     }
+
+    
+//     // to make a vector of pair elem
+
+//     vector<pair<int,int>>vec_pair;
+//     vec_pair.push_back({1,2});
+
+
+//     // to print the vector using for loop 
+//     for(int j=0;j<vec_pair.size();j++){
+//         cout<<vec_pair[j].first<<vec_pair[j].second<<endl;
+//     }
+    
+
+
+//     // to make a vector or many elem with declaring a times of elem
+//     vector<int>v1(5,100);
+//     vector<int>v2(v1);
+
+//     for(int z=0;z<v2.size();z++){
+//         cout<<v2[z]<<endl; 
+//     }
+
