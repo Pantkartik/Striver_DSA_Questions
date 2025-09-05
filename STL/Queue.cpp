786fd154af62c64d45e31626579b758cccdc75a1
Kartik Pant <kartikpant.kp69@gmail.com>
2025-09-06 00:04:53 +0530

    queue

---
+++ STL/Queue.cpp
@@ -0,0 +1,30 @@
+//  A queue is same as stack but FIFO ( first in first out )
+
+#include<bits/stdc++.h>
+using namespace std; 
+
+int main(){
+
+
+    queue<int>que;
+
+
+    que.push(5);
+    que.push(10);
+    que.push(15);
+    que.push(20);
+    que.push(25);
+
+    cout<<que.front()<<endl;
+
+    // deleting front and checking for the next in line 
+
+    que.pop();
+
+    cout<<que.front()<<endl;
+    cout<<que.back()<<endl;
+
+    // this proves that the queue is a fifo 
+
+
+}
