ff2954855ab16d3f064f7c7c0d31cf49ad07da1b
Kartik Pant <kartikpant.kp69@gmail.com>
2025-09-28 22:17:08 +0530

    bubble_sort

--- Sorting/Bubble_Sort/tempCodeRunnerFile.cpp
+++ Sorting/Bubble_Sort/tempCodeRunnerFile.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 void bubble_sort(int array[],int n){
     for(int i=n-1;i>=0;i--){
-        for(int j=0;j>i-1;j++){
+        for(int j=0;j>=i-1;j++){
             if(array[j]>array[j+1])  {
                 swap(array[j],array[j+1]);
             }
