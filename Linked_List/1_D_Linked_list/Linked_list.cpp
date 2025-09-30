cf90e0544b9de306de6b333891246e467b089e25
Kartik Pant <kartikpant.kp69@gmail.com>
2025-09-30 21:27:56 +0530

    Update Linked_list.cpp

--- Linked_List/1_D_Linked_list/Linked_list.cpp
+++ Linked_List/1_D_Linked_list/Linked_list.cpp
@@ -20,3 +20,41 @@ int main(){
     cout<<y<<endl;
     cout<<y->data<<endl;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#include<bits/stdc++.h>
+using namespace std; 
+
+
+struct node{
+
+public:
+    int data;
+    node*next;
+public:
+    node(int data,node*next){
+        data=data1;
+        next=next1;
+    }
+};
+
+int main(){
+    vector<int>arr={1,2,3,4,5,67,};
+    node*y=new node(arr[0],nullptr);
+    cout<<y->data<<endl;
+}
