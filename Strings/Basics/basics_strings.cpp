a96754c9617bc47fae87ceed1aa3a916a7d20d89
Kartik Pant <kartikpant.kp69@gmail.com>
2025-10-10 22:17:04 +0530

    strings_basics

---
+++ Strings/Basics/basics_strings.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    // to get the input string from user 
+    char name[20];
+    cin>>name;
+    cout<<name;
+
+
+    //  to calculate the length of string 
+    int len=name.size();
+    cout<<len<<endl;
+}
