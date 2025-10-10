f79fec95c9561b311d1c01af8a76d7ff3e8dc7b6
Kartik Pant <kartikpant.kp69@gmail.com>
2025-10-10 21:52:05 +0530

    correction_question

--- Binary_Search/LEETCODE_QUESTION/Hard/Find_single_duplicate_in sorted array_with binary_search.cpp
+++ Binary_Search/LEETCODE_QUESTION/Hard/Find_single_duplicate_in sorted array_with binary_search.cpp
@@ -1,7 +1,39 @@
 // to find the duplicate single element in an array using the binary search 
 
 
-#include<bits/c++.h>
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int find_single(vector<int>nums,int n){
+    // edge cases 
+    if(nums[0]!=nums[1])return 0;
+    if(nums[n-1]!=nums[n-2])return n-1;
+    int start=1,end=n-2;
+    while(start<=end){
+        int mid=start+(end-start)/2;
+        if(nums[mid]==nums[mid-1] && nums[mid]==nums[mid+1])return mid;
+        else if(mid%2==1 && nums[mid]==nums[mid-1]){
+            start=mid+1;
+        }
+        else {
+            end=mid-1;
+        }
+    } 
+}
+
+int main(){
+    vector<int>nums={1,1,2,2,3,3,4,5,5,6,6,7,7,8,8};
+    int n=nums.size();
+ 
+    cout<<find_single(nums,n);
+}
+
+
+
+
+
+#include<iostream>
 using namespace std;
 
 
@@ -13,7 +45,7 @@ int find_dup(vector<int>arr,int n){
         int start=1,end=n-2;
         while(start<=end){
             int mid=start(end-start)/2;
-            if(arr[mid]!=arr[mid-1]   && arr[mid]!=arr[mid+1])return arr[mid];
+            if(arr[mid]!=arr[mid-1]   && arr[mid]!=arr[mid+1])return mid;
             if(mid%2==1 && arr[mid-1]==arr[mid]) start=mid+1;
             else end=mid-1;
         }
@@ -25,3 +57,5 @@ int main(){
     cout<<"The duplicate elem index  : "<<find_dup(arr,n);
 
 }
+
+
