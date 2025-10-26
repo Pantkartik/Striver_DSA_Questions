195bc8186b594711b36698f92c7342335bc6e173
Kartik Pant <kartikpant.kp69@gmail.com>
2025-10-26 22:23:08 +0530

    Unordered_map

---
+++ Hashing/Unordered_map/Unordered_map_code.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int arr[10]={1,1,2,3,4,5,4,2,7,8};
+    int size=10;
+
+    // declaring the map 
+
+    map<int,int>mpp;
+    for(int i=0;i<size;i++){
+        mpp[arr[i]]++;
+
+        // pre computing the arr element in the map as key:value pair 
+    }
+
+    // calling the map
+
+    int number;
+    cout<<"Enter the number of element to check freq."<<endl;
+    cin>>number;
+
+    while(number--){
+        int query;
+        cout<<"Enter the number to find frquencies  : "<<endl;
+        cin>>query;
+
+        // calling the map 
+
+        cout<<mpp[query]<<endl;
+    }
+}
